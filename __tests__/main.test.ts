9fcdf1b40f84c9e47ea2847bded94602637aafcd
Nils Diekmann <github@kinneko.de>
2024-07-29 00:28:43 +0200

    Initial version of the github action (#5)
    - initial version like described in my article
    - test for initial version

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,89 +1,185 @@
-/**
- * Unit tests for the action's main functionality, src/main.ts
- *
- * These should be run as if the action was called from a workflow.
- * Specifically, the inputs listed in `action.yml` should be set as environment
- * variables following the pattern `INPUT_<INPUT_NAME>`.
- */
-
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import * as main from '../src/main'
+
+// Mock the GitHub Actions core library
+// let debugMock: jest.SpiedFunction<typeof core.debug>
+// let errorMock: jest.SpiedFunction<typeof core.error>
+// let getInputMock: jest.SpiedFunction<typeof core.getInput>
+let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
+// let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
+const repoOwner = 'mockOwner'
+const repoName = 'mockRepo'
+
+jest.mock('@actions/core')
+jest.mock('@actions/github')
 
 // Mock the action's main function
 const runMock = jest.spyOn(main, 'run')
-
-// Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/
-
-// Mock the GitHub Actions core library
-let debugMock: jest.SpiedFunction<typeof core.debug>
-let errorMock: jest.SpiedFunction<typeof core.error>
-let getInputMock: jest.SpiedFunction<typeof core.getInput>
-let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
-let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
 
 describe('action', () => {
   beforeEach(() => {
     jest.clearAllMocks()
 
-    debugMock = jest.spyOn(core, 'debug').mockImplementation()
-    errorMock = jest.spyOn(core, 'error').mockImplementation()
-    getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
+    // debugMock = jest.spyOn(core, 'debug').mockImplementation()
+    // errorMock = jest.spyOn(core, 'error').mockImplementation()
+    jest.spyOn(core, 'getInput').mockImplementation(name => {
+      if (name === 'token') {
+        return 'ghp_mockGithubToken123'
+      }
+      return ''
+    })
     setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
-    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+    // setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+
+    // Mock GitHub context
+    Object.defineProperty(github, 'context', {
+      get: () => ({
+        repo: {
+          owner: repoOwner,
+          repo: repoName
+        }
+      })
+    })
   })
 
-  it('sets the time output', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation(name => {
-      switch (name) {
-        case 'milliseconds':
-          return '500'
-        default:
-          return ''
+  it('deletes a tag for a non-existing branch', async () => {
+    const tagTobeDeleted = 'v1.0.1-iamnotthereanymore.1'
+
+    const octokitMock = {
+      rest: {
+        repos: {
+          listBranches: jest.fn().mockResolvedValue({
+            data: [{ name: 'main' }]
+          })
+        },
+        git: {
+          listMatchingRefs: jest.fn().mockResolvedValue({
+            data: [{ ref: `refs/tags/${tagTobeDeleted}` }]
+          }),
+          deleteRef: jest.fn().mockResolvedValue({})
+        }
       }
-    })
+    }
+    ;(github.getOctokit as jest.Mock).mockReturnValue(octokitMock)
 
     await main.run()
-    expect(runMock).toHaveReturned()
 
-    // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
-    )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
-    expect(errorMock).not.toHaveBeenCalled()
+    // Assuming a function to check tag-to-branch association is implemented
+    // and tags v1.0.0 and v1.0.1 do not correspond to any existing branch
+    expect(octokitMock.rest.git.deleteRef).toHaveBeenCalledWith({
+      owner: repoOwner,
+      repo: repoName,
+      ref: `tags/${tagTobeDeleted}`
+    })
   })
 
-  it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation(name => {
-      switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
-        default:
-          return ''
+  it('does not delete a tag for an existing branch', async () => {
+    const tagToBeNotDeleted = 'v1.0.1-iamstillhere.1'
+
+    const octokitMock = {
+      rest: {
+        repos: {
+          listBranches: jest.fn().mockResolvedValue({
+            data: [{ name: 'main' }, { name: 'feature/iamstillhere' }]
+          })
+        },
+        git: {
+          listMatchingRefs: jest.fn().mockResolvedValue({
+            data: [{ ref: `refs/tags/${tagToBeNotDeleted}` }]
+          }),
+          deleteRef: jest.fn().mockResolvedValue({})
+        }
       }
-    })
+    }
+    ;(github.getOctokit as jest.Mock).mockReturnValue(octokitMock)
 
     await main.run()
-    expect(runMock).toHaveReturned()
 
-    // Verify that all of the core library functions were called correctly
+    // Check that deleteRef was not called for tags associated with existing branches
+    expect(octokitMock.rest.git.deleteRef).not.toHaveBeenCalledWith({
+      owner: repoOwner,
+      repo: repoName,
+      ref: `tags/${tagToBeNotDeleted}`
+    })
+  })
+
+  it('never deletes tags on the main branch', async () => {
+    const tagToBeNotDeleted = 'v1.0.1'
+
+    const octokitMock = {
+      rest: {
+        repos: {
+          listBranches: jest.fn().mockResolvedValue({
+            data: [{ name: 'main' }, { name: 'feature/iamstillhere' }] // Only the data needed for this test
+          })
+        },
+        git: {
+          listMatchingRefs: jest.fn().mockResolvedValue({
+            data: [{ ref: `refs/tags/${tagToBeNotDeleted}` }]
+          }),
+          deleteRef: jest.fn().mockResolvedValue({})
+        }
+      }
+    }
+    ;(github.getOctokit as jest.Mock).mockReturnValue(octokitMock)
+
+    await main.run()
+
+    expect(octokitMock.rest.git.deleteRef).not.toHaveBeenCalledWith({
+      owner: repoOwner,
+      repo: repoName,
+      ref: `tags/${tagToBeNotDeleted}`
+    })
+  })
+
+  it('should fail if a GitHub REST call throws an error', async () => {
+    const errorMessage = 'GitHub API error'
+    const octokitMock = {
+      rest: {
+        repos: {
+          listBranches: jest.fn().mockRejectedValue(new Error(errorMessage))
+        },
+        git: {
+          listMatchingRefs: jest.fn().mockResolvedValue({
+            data: [{ ref: `refs/tags/v1.0.1}` }]
+          }),
+          deleteRef: jest.fn().mockResolvedValue({})
+        }
+      }
+    }
+
+    ;(github.getOctokit as jest.Mock).mockReturnValue(octokitMock)
+
+    await main.run()
+
+    expect(runMock).toHaveReturned()
+    expect(setFailedMock).toHaveBeenNthCalledWith(1, errorMessage)
+  })
+
+  it('should fail if a GitHub REST call throws an unknown error', async () => {
+    const octokitMock = {
+      rest: {
+        repos: {
+          listBranches: jest.fn().mockRejectedValue('unknown error')
+        },
+        git: {
+          listMatchingRefs: jest.fn().mockResolvedValue({
+            data: [{ ref: `refs/tags/v1.0.1}` }]
+          }),
+          deleteRef: jest.fn().mockResolvedValue({})
+        }
+      }
+    }
+
+    ;(github.getOctokit as jest.Mock).mockReturnValue(octokitMock)
+
+    await main.run()
+
+    expect(runMock).toHaveReturned()
     expect(setFailedMock).toHaveBeenNthCalledWith(
       1,
-      'milliseconds not a number'
+      'An unknown error occurred'
     )
-    expect(errorMock).not.toHaveBeenCalled()
   })
 })
